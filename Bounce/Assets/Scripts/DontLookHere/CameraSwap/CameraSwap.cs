d3d197199345a1ef2a53d8571b7bbb80d4ff8d24
lennystevejox <2000lennyj@gmail.com>
2020-11-18 23:47:30 +0100

    2D assets made Move2D.cs made Grounded.cs made CameraSwap.cs and CameraSwap2.cs made Level 1 made

---
+++ Bounce/Assets/Scripts/DontLookHere/CameraSwap/CameraSwap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
+    }
+}
---
+++ Bounce/Assets/Scripts/Move2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float launchForce;
+    public float moveSpeed = 5f;
+
+    public bool isGrounded = false;
+    
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+    
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("JumpPad"))
+        {
+            rb.velocity = Vector2.up * launchForce;
+        }
+    }
+}
